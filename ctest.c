66b8de082a9529d19157703e21d67e309c98f935
Nayaco <1194329594@qq,com>
2018-04-16 15:07:15 +0800

    learn socket

--- ctest.c
+++ ctest.c
@@ -10,9 +10,9 @@
 
 int main(int argc, char** argv)
 {
-    int     sockfd,         n;
+    int     sockfd,         n,             listenfd,  connfd;
     char    recvline[4096], sendline[4096];
-    struct  sockaddr_in     servaddr;
+    struct  sockaddr_in     servaddr,      cliaddr;
 
     if( argc != 2){
     printf("usage: ./client <ipaddress>\n");
@@ -24,12 +24,26 @@ int main(int argc, char** argv)
     exit(0);
     }
 
+    if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
+        printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);
+        exit(0);
+    }
+
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(6666);
+
+    memset(&cliaddr, 0, sizeof(cliaddr));
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_port = htons(2333);
     if( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
     printf("inet_pton error for %s\n",argv[1]);
     exit(0);
+    }
+
+    if( bind(listenfd, (struct sockaddr*)&cliaddr, sizeof(cliaddr)) == -1){
+        printf("bind socket error: %s(errno: %d)\n",strerror(errno),errno);
+        exit(0);
     }
 
     if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
@@ -44,7 +58,22 @@ int main(int argc, char** argv)
     printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
     exit(0);
     }
-    
+
+    if( listen(listenfd, 10) == -1){
+        printf("listen socket error: %s(errno: %d)\n",strerror(errno),errno);
+        exit(0);
+    }
+
+    if( (connfd = accept(listenfd, (struct sockaddr*)NULL, NULL)) == -1){
+        printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
+    }
+
+    n = recv(connfd, recvline, MAXLINE, 0);
+    recvline[n] = '\0';
+    close(connfd);
+    printf("\n%s\n", recvline);
+
+    close(listenfd);
     close(sockfd);
     exit(0);
 }
